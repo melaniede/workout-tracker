ddf62a0da10d7e114c23c355f56a9905739bc70a
melaniede <debarrosmelanie@gmail.com>
2020-10-25 18:49:25 -0400

    models

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        required: "Date is Required",
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Type is Required"
+        },
+
+        name: {
+            type: String,
+            trim: true,
+            required: "Name is Required",
+        },
+
+        duration: {
+            type: Number,
+            required: "Duration is Required"
+        },
+
+        weight: {
+            type: Number
+        },
+
+        reps: {
+            type: Number
+        },
+
+        sets: {
+            type: Number
+        },
+
+        distance: {
+            type: Number
+        }
+    }
+    ]
+});
+
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
