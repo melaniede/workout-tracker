625230f358d09791dbb0c464be058f1c9a330e6c
melaniede <debarrosmelanie@gmail.com>
2020-10-25 21:36:53 -0400

    update db

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/workouttrackerdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
